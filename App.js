6bd4d2fa83eac21731372f560be66cbcf5de61ad
gomgomgit <thechocohead@gmail.com>
2020-03-23 18:40:09 +0700

    tugas

---
+++ App.js
@@ -0,0 +1,202 @@
+import React from 'react';
+import { View, Text, ScrollView, StatusBar, Image, StyleSheet } from 'react-native';
+
+export default function App() {
+  return(
+      <View style={{flex:1, paddingTop: StatusBar.currentHeight || 0}}>
+        <StatusBar barStyle='dark-content'></StatusBar>
+        <View style={[sty.head,{flexDirection: 'row', width: '100%'}]}>
+          <View style={[sty.tengah, {flex:1}]}>
+            <Image
+              style={{width: 50, height: 70}}
+              source={{uri: 'https://cdn.clipart.email/4d1682843769f1df76970c4388053dc1_kobe-bryant-logo-logodix_3840-2160.png'}}
+            />
+          </View>
+          <View style={[sty.tengah,{flex:4}]}>
+            <Text style={{fontSize: 24, color: 'white', fontWeight: 'bold'}}>
+              BLACK MAMBA
+            </Text>
+          </View>
+          <View style={[sty.tengah, {flex:1}]}>
+            <Image
+              style={{width: 50, height: 70}}
+              source={{uri: 'https://cdn.clipart.email/4d1682843769f1df76970c4388053dc1_kobe-bryant-logo-logodix_3840-2160.png'}}
+            />
+          </View>
+        </View>
+        <View style={{height: 100}}>
+          <ScrollView horizontal={true}>
+            <View style={{flexDirection:'row', paddingVertical: 10}}>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+              <View style={[sty.scrHorizontal, sty.tengah]}>
+                <Text>
+                    Hey
+                </Text>
+              </View>
+            </View>
+          </ScrollView>
+        </View>
+        <View style={{flex: 8}}>
+          <ScrollView style={{width: '100%', backgroundColor: '#f9f9f9'}}>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+            <View style={[sty.mtext, sty.tengah,{height: 80, marginVertical: 15}]}>
+              <Text>
+                Content
+              </Text>
+            </View>
+          </ScrollView>
+        </View>
+        <View style={{height: 80, backgroundColor: '#222', flexDirection: 'row'}}>
+          <View style={sty.bawah}>
+            <Text style={sty.tBawah}>
+              1
+            </Text>
+          </View>
+          <View style={sty.bawah}>
+            <Text style={sty.tBawah}>
+              2
+            </Text>
+          </View>
+          <View style={sty.bawah}>
+            <Text style={sty.tBawah}>
+              3
+            </Text>
+          </View>
+          <View style={sty.bawah}>
+            <Text style={sty.tBawah}>
+              4
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+}
+
+const sty = StyleSheet.create({
+  tengah: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  head: {
+    flex: 2,
+    backgroundColor: '#2ab',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  foot: {
+    flex: 1,
+  },
+  scrHorizontal: {
+    width: 75,
+    height: 75,
+    borderWidth: 3,
+    borderColor: '#ccc',
+    margin: 5,
+    borderRadius: 12
+  },
+  mtext: {
+    margin: 16,
+    borderRadius: 12,
+    borderWidth: 3,
+    borderColor: '#ccc',
+    shadowColor: '#000',
+    shadowOffset: {width: 2, height: 2},
+    shadowOpacity: 1,
+    shadowRadius: 1,
+  },
+  bawah: {
+    backgroundColor: '#aaa',
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#fff',
+    borderWidth: 1
+  },
+  tBawah: {
+    fontSize: 40,
+  }
+});
